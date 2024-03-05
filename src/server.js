55c6bbf8124fa09e6fd61b81d75352a3b0d065e6
datata <dani@danitarazona.dev>
2024-03-05 13:07:59 +0100

    feat: router added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import 'dotenv/config'
 import { dbConnection } from "./database/db.js";
+import router from './routes/router.js'
 
 const app = express()
 
@@ -9,6 +10,18 @@ const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 4001;
+
+// API ROUTES
+app.get('/api/healthy', (req, res) => {
+  res.status(200).json(
+    {
+      success: true,
+      message: "server is healthy"
+    }
+  )
+})
+
+app.use('/api', router)
 
 dbConnection()
 .then(() =>{
