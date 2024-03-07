2e5c9e4ef7a60ca8df28c0ec850c93a547d04d5a
datata <dani@danitarazona.dev>
2024-03-07 13:08:19 +0100

    feat: logic book create

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,8 +1,42 @@
+import Book from "../models/Book.js"
+
 export const createBook = async(req, res) => {
-  res.status(201).json(
-    {
-      success: true,
-      message: "Book created"
+  try {
+    const { title, description, author } = req.body
+    // const title = req.body.title
+
+    if(!title || !description || !author) {
+      return res.status(400).json(
+        {
+          success: false,
+          message: "title description and author required"
+        }
+      )
     }
-  )
+
+    const newBook = await Book.create(
+      {
+        // title: title
+        title,
+        description,
+        author
+      }
+    )
+
+    res.status(201).json(
+      {
+        success: true,
+        message: "Book created",
+        data: newBook
+      }
+    )
+  } catch (error) {
+    res.status(500).json(
+      {
+        success: false,
+        message: "Book cant created",
+        error: error.message
+      }
+    )
+  }
 }
